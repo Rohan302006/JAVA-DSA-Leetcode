da68120021506bc2d45485eb4c978985c94426d4
Rohan Hede <hederohan4@gmail.com>
2026-02-02 21:03:42 +0530

    Added solution for Valid Parenthesis problem (Leetcode Q.20)

---
+++ Stack/Valid_Paranthesis.java
@@ -0,0 +1,65 @@
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+        Stack<Character> stack = new Stack();
+        for (char i : s.toCharArray()) 
+            {
+            if (isOpenBracket(i)) 
+            {
+                stack.push(i);
+            } else 
+                {
+                if (stack.size() == 0) 
+                {
+                    return false;
+                } 
+                else 
+                {
+                    if (isMatchingBracket(stack.peek(), i)) 
+                    {
+                        stack.pop();
+                    } 
+                    else 
+                    {
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return stack.size() == 0;
+    }
+
+
+    private boolean isOpenBracket(char c) 
+    {
+        switch (c) 
+        {
+            case '{':
+            case '[':
+            case '(':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    
+    private boolean isMatchingBracket(char c, char d) 
+    {
+        if (c == '(' && d == ')') 
+        {
+            return true;
+        } else if (c == '{' && d == '}') 
+        {
+            return true;
+        } else if (c == '[' && d == ']') 
+        {
+            return true;
+        } else 
+        {
+            return false;
+        }
+    }
+}
