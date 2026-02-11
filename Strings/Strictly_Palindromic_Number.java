4c1b731f8d9bd0ffe5ba25fdbedfebfed7d13534
Rohan Hede <hederohan4@gmail.com>
2026-02-11 19:28:26 +0530

    Added solution for Strictly Palindromic Number (Leetcode Q. 2396)

---
+++ Strings/Strictly_Palindromic_Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        
+        return false;
+
+    /* For all n ≥ 4, the answer is ALWAYS false.
+    Because:
+    for example:-
+    In base n - 2, the number n is always written as "12"
+    "12" is not a palindrome
+    So:
+    There is no number n ≥ 4 can be strictly palindromic.
+    & since the constraints are given as n >= 4 therefore the result will be always false */
+
+    }
+}
