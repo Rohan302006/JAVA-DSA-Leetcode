990f741c54537f2faf5102f9626f1918be4210af
Rohan Hede <hederohan4@gmail.com>
2026-02-26 19:36:29 +0530

    Added solution for Count Vowel Substrings of a String problem (Leetcode Q. 2062)

---
+++ Strings/Count_Vowel_Substrings_Of_A_String.java
@@ -0,0 +1,32 @@
+class Solution 
+{
+    public int countVowelSubstrings(String word) 
+    {
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        int n = word.length();
+        
+        for (int i = 0; i < n - 4; i++) 
+        {
+            set.clear();
+            for (int j = i; j < n; j++) 
+            {
+                char ch = word.charAt(j);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+                {
+                    set.add(ch);
+
+                    if (set.size() == 5) 
+                    {
+                        count++;
+                    }
+                } 
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
