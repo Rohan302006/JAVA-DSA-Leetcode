d7a9f55159d18ee9cd8ff3fade5150ea81ed457b
Rohan Hede <hederohan4@gmail.com>
2026-03-13 21:06:47 +0530

    Added solution for Count the number of consistent strings  problem (Leetcode Q.1684)

---
+++ HashMap & HashSet/Count_The_Number_Of_Consistent_Strings.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int countConsistentStrings(String allowed, String[] words) 
+    {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < allowed.length(); i++) 
+        {
+            set.add(allowed.charAt(i));
+        }
+
+        int count = 0;
+        for (String k : words) 
+        {
+            int f = 1;
+            for (int i = 0; i < k.length(); i++) 
+            {
+                if (!set.contains(k.charAt(i))) 
+                {
+                    f = 0;
+                    break;
+                }
+            }
+            count += f;
+        }
+        return count;
+    }
+}
