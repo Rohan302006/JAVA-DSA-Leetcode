ef44c2e03fcfa4e4bb648a0e36ca74bf53c6106a
Rohan Hede <hederohan4@gmail.com>
2026-05-03 20:05:00 +0530

    Added solution for Find the Highest Altitude problem (Leetcode Q. 1732)

---
+++ Arrays/Find_The_Highest_Altitude.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int largestAltitude(int[] gain) 
+    {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < gain.length; i++) 
+        {
+            sum += gain[i];
+            max = Math.max(max, sum);
+        }
+        return max > 0 ? max : 0;
+    }
+}
