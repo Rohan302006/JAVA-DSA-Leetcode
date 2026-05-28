ab6f9325c8fcfba780fdb4a171efaf3bee1aa764
Rohan302006 <hederohan4@gmail.com>
2026-05-28 11:19:47 +0530

    Added solution for Maximum Ascending Subarray Sum problem (Leetcode Q. 1800)

---
+++ Arrays/Maximum_Ascending_Subarray_Sum.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int maxAscendingSum(int[] nums) 
+    {
+        int sum = nums[0];
+        int MaxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) 
+        {
+
+            if (nums[i] > nums[i - 1]) 
+            {
+                sum += nums[i];
+            } 
+            else 
+            {
+                sum = nums[i];
+            }
+            
+            MaxSum = Math.max(MaxSum, sum);
+        }
+        return MaxSum;
+    }
+}
