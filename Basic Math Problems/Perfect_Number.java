b55f6235ed6310618488ce0f9e46cf66a2a185e5
Rohan302006 <hederohan4@gmail.com>
2026-06-08 08:50:58 +0530

    Added solution for Perfect Number problem (Leetcode Q.507)

---
+++ Basic Math Problems/Perfect_Number.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public boolean checkPerfectNumber(int num) 
+    {
+        int temp = num;
+        int sum = 0;
+        for (int i = 1; i < num; i++) 
+        {
+            if (num % i == 0) 
+            {
+                sum += i;
+            }
+            if (sum > temp) 
+            {
+                return false;
+            }
+        }
+        return sum == temp;
+    }
+}
