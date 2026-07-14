2b26b273c66eca20e320c3763cc91998c9168888
Rohan302006 <hederohan4@gmail.com>
2026-07-14 18:56:59 +0530

    Added solution for Intersection of Two Linked Lists problem (Leetcode Q. 160)

---
+++ Linked List/Intersection_Of_Two_Linked_LIsts.java
@@ -0,0 +1,31 @@
+public class Solution 
+{
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    {
+
+        ListNode pointer1 = headA;
+        ListNode pointer2 = headB;
+
+        while (pointer1 != pointer2) 
+        {
+            if (pointer1 == null) 
+            {
+                pointer1 = headB;
+            } 
+            else 
+            {
+                pointer1 = pointer1.next;
+            }
+
+            if (pointer2 == null) 
+            {
+                pointer2 = headA;
+            } 
+            else 
+            {
+                pointer2 = pointer2.next;
+            }
+        }
+        return pointer2;
+    }
+}
