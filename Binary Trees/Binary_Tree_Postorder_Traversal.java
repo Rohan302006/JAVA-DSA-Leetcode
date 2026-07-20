4fc6b6d94579e75ccddf044f202f43559cb9be7d
Rohan302006 <hederohan4@gmail.com>
2026-07-20 19:32:33 +0530

    Added solution for Binary Tree Postorder Traversal problem (Leetcode Q. 145)

---
+++ Binary Trees/Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public List<Integer> postorderTraversal(TreeNode root) 
+    {
+        List<Integer> ans = new ArrayList<>();
+        postOrder(root, ans);
+        return ans;
+    }
+
+    private void postOrder(TreeNode root, List<Integer> ans) 
+    {
+        if (root == null) 
+        {
+            return;
+        }
+
+        postOrder(root.left, ans);
+        postOrder(root.right, ans);
+        ans.add(root.val);
+    }
+}
