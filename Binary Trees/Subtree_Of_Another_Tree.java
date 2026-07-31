1b6727ca80dd1ba641d0462e22234da7880ec348
Rohan302006 <hederohan4@gmail.com>
2026-07-31 21:45:01 +0530

    Added solution for Subtree of Another Tree problem (Leetcode Q. 572)

---
+++ Binary Trees/Subtree_Of_Another_Tree.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) 
+    {
+        if (root == null || subRoot == null) 
+        {
+            return root == subRoot;
+        }
+
+        if (root.val == subRoot.val && isSameTree(root, subRoot)) 
+        {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+    }
+    // Same tree function (question number 100)
+
+    public boolean isSameTree(TreeNode p, TreeNode q) 
+    {
+        if (p == null || q == null) 
+        {
+            return p == q;
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
