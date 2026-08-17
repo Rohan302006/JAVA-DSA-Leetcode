ecad7b44961d80cbad8799489e6915c02aee99ce
Rohan302006 <hederohan4@gmail.com>
2026-08-17 18:51:35 +0530

    Added implementation of BSTIterator for in-order traversal of a binary search tree (Leetcode Q. 173)

---
+++ Binary Search Trees/Binary_Search_Tree_Iterator.java
@@ -0,0 +1,27 @@
+class BSTIterator {
+
+    Stack<TreeNode> st = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    public int next() {
+        TreeNode node = st.pop();
+
+        pushAll(node.right);
+
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    public void pushAll(TreeNode node) {
+        while (node != null) {
+            st.push(node);
+            node = node.left;
+        }
+    }
+}
