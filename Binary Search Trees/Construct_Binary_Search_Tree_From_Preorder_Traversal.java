328fd47c54b9a288a6b10d4ca514aa146aa88512
Rohan302006 <hederohan4@gmail.com>
2026-08-24 23:03:27 +0530

    Added solution for Construct Binary Search Tree from Preorder Traversal problem (Leetcode Q. 1008)

---
+++ Binary Search Trees/Construct_Binary_Search_Tree_From_Preorder_Traversal.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    int i = 0;
+
+    public TreeNode bstFromPreorder(int[] preorder) 
+    {
+        return buildTree(preorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode buildTree(int[] preorder, int bound)
+    {
+        if (i >= preorder.length || preorder[i] > bound) 
+        {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[i]);
+        i++;
+
+        root.left = buildTree(preorder, root.val);
+        root.right = buildTree(preorder, bound);
+
+        return root;
+    }
+}
