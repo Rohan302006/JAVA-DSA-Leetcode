4caf56b738cfe0ca8622fe6e57d617e91cc6f9af
Rohan302006 <hederohan4@gmail.com>
2026-09-05 19:40:33 +0530

    Added solution for String Compression problem (Leetcode Q. 443)

---
+++ Strings/String_Compression.java
@@ -0,0 +1,36 @@
+class Solution 
+{
+    public int compress(char[] chars) 
+    {
+        int n = chars.length;
+
+        int i = 0;
+        int idx = 0;
+
+        while (i < n) 
+        {
+            char curr = chars[i];
+
+            int count = 0;
+            while (i < n && chars[i] == curr) 
+            {
+                i++;
+                count++;
+            }
+
+            chars[idx++] = curr;
+
+            if (count > 1) 
+            {
+                String countStr = Integer.toString(count);
+                
+                for (char ch : countStr.toCharArray()) 
+                {
+                    chars[idx++] = ch;
+                }
+            }
+        }
+
+        return idx;
+    }
+}
