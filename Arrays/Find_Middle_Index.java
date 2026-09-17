8c6ff64d7af4706a737e5987d2953192c0b6a3e2
Rohan302006 <hederohan4@gmail.com>
2026-09-17 21:18:38 +0530

    Added solution for Find Middle Index problem (Leetcode Q. 1991)

---
+++ Arrays/Find_Middle_Index.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public int findMiddleIndex(int[] nums) 
+    {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) 
+        {
+            int left_sum = 0;
+            int right_sum = 0;
+
+            for (int j = 0; j < i; j++) 
+            {
+                left_sum += nums[j];
+            }
+
+            for (int j = i + 1; j < n; j++) 
+            {
+                right_sum += nums[j];
+            }
+
+            if (left_sum == right_sum) 
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
