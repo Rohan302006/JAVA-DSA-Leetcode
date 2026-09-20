dd649f822905b52a25e2107cd390f47ad4e37475
Rohan302006 <hederohan4@gmail.com>
2026-09-20 20:45:59 +0530

    Added solution for Reverse Degree of a String problem (Leetcode Q. 3498)

---
+++ Strings/Reverse_Degree_Of_A_String.java
@@ -0,0 +1,41 @@
+class Solution 
+{
+    public int reverseDegree(String s) 
+    {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char ch = s.charAt(i);
+
+            int value = 26 - (ch - 'a');
+            int position = i + 1;
+
+            sum += value * position;
+        }
+        return sum;
+    }
+}
+
+
+/*  Same Approach  Different Formula :-  'z' - ch + 1 
+
+class Solution {            
+    public int reverseDegree(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            int value = 'z' - ch + 1;  
+
+            int position = i+1;
+            sum += value * position;
+        }
+
+        return sum;
+    }
+
+}
+
+*/
